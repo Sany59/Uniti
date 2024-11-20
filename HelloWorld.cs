c6e240c1b575c447f285374d9630715d7dbc93a3
Sany59 <sultaleks2030@gmail.com>
2024-11-20 20:03:54 +0500

    Add files via upload
    добавил приветствующую фразу

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //1 раз в начале игры
+    void Start()
+    {
+        print("HelloWorld");
+    }
+
+    // Update is called once per frame
+    //выполняется много раз
+    void Update()
+    {
+        
+    }
+}
